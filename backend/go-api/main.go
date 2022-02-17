8b7b04b4f82d5c431f6b45110e3d1eb97bfe1988
hulewiczkamil <hulewicz.k@gmail.com>
2022-02-17 23:39:06 +0100

    Add second endpoint to configuration

--- backend/go-api/main.go
+++ backend/go-api/main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Testing golang application with docker 🔥")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// log.Println("Testing golang application with docker 🔥")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "An arror occured", http.StatusBadRequest)
+			return
+			//Alternative version
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("An error occured"))
+			// return
+		}
+
+		log.Printf("Data %s\n", d)
+
+		fmt.Fprintf(rw, "I have read data %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World :3")
 	})
 
 	fmt.Printf("Started working!\n")
