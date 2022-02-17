49ee5970ba229a905b9578638af185c9ba855ae1
hulewiczkamil <hulewicz.k@gmail.com>
2022-02-17 23:04:27 +0100

    Add basic http handling functionality, update Dockerfile

--- backend/go-api/main.go
+++ backend/go-api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Testing golang app with docker 🔥")
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("Testing golang application with docker 🔥")
+	})
+
+	fmt.Printf("Started working!\n")
+	http.ListenAndServe(":8080", nil)
+	fmt.Print("Stopped working!\n")
 }
